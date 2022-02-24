0085187a4d0cf6420d46569c32f65ca86e96500d
Kanokwan <kanokwan.asher@gmail.com>
2022-02-23 21:40:50 -0500

    aa

--- github_day1.java
+++
@@ -1,5 +0,0 @@
-public class github_day1 {
-    public static void main(String[] args) {
-        System.out.println("Github First Java Project");
-    }
-}
---
+++ src/github_day1/GithubFirstJava.java
@@ -0,0 +1,7 @@
+package github_day1;
+
+public class GithubFirstJava {
+    public static void main(String[] args) {
+        System.out.println("My Github First Java Project");
+    }
+}
